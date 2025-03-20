b49d3cfc3ad684ccbcc5d0c0119d1285b6246c51
Rehtt <dsreshiram@gmail.com>
2025-03-20 08:47:37 +0800

    shell

---
+++ cloudcone_start.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
+docker login
+
+apt update && apt install tmux
+
+dd if=/dev/zero of=/var/swapfile bs=1M count=4096
+mkswap /var/swapfile
+swapon /var/swapfile
+
+tmux new-session -d -s mysession "docker build -t rehtt/comfy-chroma-v14 . && docker push rehtt/comfy-chroma-v14 && shutdown now"
--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-
-
-docker build -t rehtt/comfy-chroma-v14 . &&
-docker push rehtt/comfy-chroma-v14 &&
-shutdown now
