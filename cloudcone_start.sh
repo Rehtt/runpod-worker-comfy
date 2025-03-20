a519acd17da2c61c9510f0f0a16e7ee866451d03
Rehtt <dsreshiram@gmail.com>
2025-03-20 10:32:41 +0800

    fix

--- cloudcone_start.sh
+++ cloudcone_start.sh
@@ -11,4 +11,4 @@ dd if=/dev/zero of=/var/swapfile bs=1M count=4096
 mkswap /var/swapfile
 swapon /var/swapfile
 
-tmux new-session -d -s mysession "docker build -t rehtt/comfy-chroma-v14 . && docker push rehtt/runpod-worker-comfy:chroma-v14 && shutdown now"
+tmux new-session -d -s mysession "docker build -t rehtt/runpod-worker-comfy:chroma-v14 . && docker push rehtt/runpod-worker-comfy:chroma-v14 && shutdown now"
